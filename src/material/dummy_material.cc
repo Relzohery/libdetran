8e4ebc7d54c5aae6e4fcd86fb4bcf53057b4fbae
Relzohery <rabab.elzohery@gmail.com>
2020-07-26 19:43:22 -0500

    some changes

--- src/material/dummy_material.cc
+++ src/material/dummy_material.cc
@@ -9,10 +9,12 @@
 #include <iostream>
 
 #include "dummy_material.hh"
+#include "utilities/Warning.hh"
+namespace detran_material
+{
 dummy_material::dummy_material(int a)
 {
-
-	std::cout << "it's working .........." << "\n";
-
+  std::cout << "it's working .........." << "\n";
 }
 
+}
