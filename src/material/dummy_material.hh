8fe1d8fc69532aa27446200fbe4a59d69efa00e0
Relzohery <rabab.elzohery@gmail.com>
2020-07-26 19:43:03 -0500

    some changes

--- src/material/dummy_material.hh
+++ src/material/dummy_material.hh
@@ -1,18 +1,20 @@
-#ifndef DETRAN_MATERIAL_HH_
-#define DETRAN_MATERIAL_HH_
+#ifndef detran_material_MATERIAL_HH_
+#define detran_material_MATERIAL_HH_
+#include "material/material_export.hh"
+#include "utilities/Definitions.hh"
+#include "utilities/SP.hh"
+#include <string>
 
-#include "Material.hh"
 
-class dummy_material
+namespace detran_material
+{
+class  dummy_material
 {
 public:
 	dummy_material(int a);
 
-
-
-
 };
-
+}
 #endif // DETRAN_MATERIAL_HH_ 
 
 //---------------------------------------------------------------------------//
